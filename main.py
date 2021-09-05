143610c257ea0c4709aace59481aa52c7c033afa
Buor <buor0@mail.ru>
2021-09-05 11:01:43 +0300

    Added one kyu6 kata

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # example of some functions
 
-from kyu6.find_the_odd_int import find_it
+from kyu6.duplicate_encoder import duplicate_encode
 
-print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
+print(duplicate_encode('Success'))
