941afcc827d9b395a001685ed0d2a04faf0b86a5
Buor <buor0@mail.ru>
2021-09-05 11:04:39 +0300

    Added link to previous kata

--- kyu6/duplicate_encoder.py
+++ kyu6/duplicate_encoder.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+
 def duplicate_encode(word):
     word = word.lower()
     return ''.join('(' if word.count(x) == 1 else ')' for x in word)
